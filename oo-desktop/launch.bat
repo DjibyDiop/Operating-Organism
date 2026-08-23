3b35bceb78b717b4b186238ffd927dff8214f7d1
npdji <npdji@users.noreply.github.com>
2026-08-23 22:53:39 +0000

    Revert "Add oo-desktop scaffold and oo-vision-core prototype"
    This reverts commit 6bf2ec12ecb7e76129e2cf9451d13d758b178a67.

--- oo-desktop/launch.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Starting oo-desktop...
-if exist "..\oo-run.ps1" (
-  powershell -ExecutionPolicy Bypass -File "..\oo-run.ps1"
-) else (
-  echo No ..\oo-run.ps1 found. See README.md
-)
